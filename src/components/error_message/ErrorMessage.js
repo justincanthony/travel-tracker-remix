6adf84616088ccd0762e6e851df09ff1b03eaa5f
Justin Anthony <justincanthony@yahoo.com>
2021-09-21 18:50:36 -0600

    Create basic strucutre for functional component

---
+++ src/components/error_message/ErrorMessage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './ErrorMessage.css'
+
+export const ErrorMessage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+
+export const Footer = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
