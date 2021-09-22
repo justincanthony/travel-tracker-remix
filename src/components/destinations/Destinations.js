c45fb510f7bc64c81edfc6909450219f0b25a2a6
Justin Anthony <justincanthony@yahoo.com>
2021-09-21 18:57:06 -0600

    Send the destination data to cards

--- src/components/destinations/Destinations.js
+++ src/components/destinations/Destinations.js
@@ -1,24 +1,36 @@
-import React, { useState, useEffect } from 'react'
-import { fetchdata } from '../../apiCalls'
+import React, { useState, useEffect } from 'react';
+import { fetchdata } from '../../apiCalls';
+import TripCard from '../trip_cards/TripCard';
+import './Destinations.css';
 
-export const Destinations = () => {
-  const [destinations, setDestinations] = useState()
-  const [error, setError] = useState()
+const Destinations = () => {
+  const [destinations, setDestinations] = useState([])
+  const [error, setError] = useState('')
 
   const getDestinations = () => {
     fetchdata()
-    .then(data => setDestinations(data.data))
+    .then(data =>  setDestinations(data.destinations))
     .catch(error => setError(error.message))
   }
+
+  const destinationCards = destinations.map((destinationObj) =>  <TripCard  key ={destinationObj.id} destinationObj={destinationObj} />)
+
 
   useEffect(() => {
     getDestinations()
   }, [])
 
-  
+
   return (
-    <div>
-      
-    </div>
+    <div>{destinationCards}</div>
   )
 }
+
+export default Destinations
+
+
+
+
+// fetch("https://xivapi.com/Action/123", {mode: 'cors'})
+    // .then(response => response.json())
+    // .then(data => console.log(data))
