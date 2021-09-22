c362e4d261aa884f2cf10d0c810c992f07304ba0
Justin Anthony <justincanthony@yahoo.com>
2021-09-22 00:47:37 -0600

    Add elements to the nav bar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-	return <div></div>;
+	return (
+		<header className="header">
+			<h1 className="appTitle">Travel Tracker v2</h1>
+			<div className="linksContainer">
+				<ul className="linksList">
+					<li>
+						<Link className="links" to="">
+							Destinations
+						</Link>
+					</li>
+					<br />
+					<li>
+						<Link className="links" to="">
+							Pending Trips
+						</Link>
+					</li>
+					<br />
+					<li>
+						<Link className="links" to="">
+							Past Trips
+						</Link>
+					</li>
+				</ul>
+			</div>
+		</header>
+	);
 };
 
 export default Navbar;
