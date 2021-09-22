02e342f6a6cc8d20817153a1f932faf4168d934a
Justin Anthony <justincanthony@yahoo.com>
2021-09-21 18:56:32 -0600

    Render destination component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,10 @@
-
+import Destinations from "../destinations/Destinations";
+import './App.css';
 
 const App = () => {
   return (
     <div className="App">
+      <Destinations/>
     </div>
   );
 }
