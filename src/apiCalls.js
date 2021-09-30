8e2cc7365d6f8dd37ab1a59f41b1b5991b71ab41
Justin Anthony <justincanthony@yahoo.com>
2021-09-30 17:29:06 -0600

    Make dynamic

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,4 @@
-export const fetchdata = async () => {
-	const res = await fetch('http://localhost:3001/api/v1/destinations');
+export const fetchdata = async (path) => {
+	const res = await fetch(`http://localhost:3001/api/v1/${path}`);
 	return res.json();
 };
