455d467ff8ef1d9b8a2eca637a06efb7d6e486f1
Justin Anthony <justincanthony@yahoo.com>
2021-09-30 17:54:20 -0600

    Create pending trips and functions

--- src/components/pendting_trips/PendingTrips.js
+++ src/components/pendting_trips/PendingTrips.js
@@ -1,5 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchdata } from '../../apiCalls';
+import TripCard from '../trip_cards/TripCard';
 
-export const PendingTrips = () => {
-	return <div></div>;
+export const PendingTrips = ({ pendingTrips }) => {
+	const { pending_trips, id } = pendingTrips;
+
+	const [tripsPending, setTripsPending] = useState([]);
+	const [error, setError] = useState('');
+
+	const getPendingTrips = () => {
+		fetchdata(pending_trips, id)
+			.then((data) => console.log(data))
+			.catch((error) => setError(error.message));
+	};
+
+	useEffect(() => {
+		getPendingTrips();
+	}, []);
+
+	const tripsPendingCards = tripsPending.map((trip) => (
+		<TripCard key={trip.id} trip={trip} />
+	));
+
+	return (
+		<section>
+			<h2>Pending Trips</h2>
+			{tripsPendingCards}
+		</section>
+	);
 };
