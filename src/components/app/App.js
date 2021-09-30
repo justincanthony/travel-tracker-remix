1df1574c1de7fa7868e193930df1df26f27054f7
Justin Anthony <justincanthony@yahoo.com>
2021-09-30 17:29:17 -0600

    Add switch and route

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,22 @@
 import Destinations from '../destinations/Destinations';
 import Navbar from '../navbar/Navbar';
+import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 const App = () => {
 	return (
 		<div className="App">
 			<Navbar />
-			<Destinations />
+			<Switch>
+				<Route
+					exact
+					path="/:destinations"
+					render={({ match }) => {
+						const { params } = match;
+						return <Destinations destinations={params.destinations} />;
+					}}
+				/>
+			</Switch>
 		</div>
 	);
 };
