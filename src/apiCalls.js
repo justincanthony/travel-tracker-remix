f183cf60db4b99924123d02f56dc2535537a391f
Justin Anthony <justincanthony@yahoo.com>
2021-09-30 18:20:39 -0600

    Add fetch call for users trips

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,8 @@ export const fetchdata = async (path) => {
 	const res = await fetch(`http://localhost:3001/api/v1/${path}`);
 	return res.json();
 };
+
+export const fetchTripsByID = async (id) => {
+	const res = await fetch(`http://localhost:3001/api/v1/trips/${id}`);
+	return res.json();
+};
