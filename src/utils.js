0b43fdf3654bf010bc1f779b94b1c02ac07d0931
Justin Anthony <justincanthony@gmail.com>
2021-10-01 12:00:12 -0600

    Import dayjs

--- src/utils.js
+++ src/utils.js
@@ -1,30 +1,32 @@
 export const filterData = {
-	getPendingTrips(data) {
-		const pendingTrips = data.filter((trip) => trip.status === 'pending');
+  getPendingTrips(data) {
+    const pendingTrips = data.filter((trip) => trip.status === 'pending');
 
-		if (pendingTrips.length < 1) {
-			return 'You do not have any current trip requests. Please visit Destinations to book your next trip!';
-		} else {
-			return pendingTrips;
-		}
-	},
+    if (pendingTrips.length < 1) {
+      return 'You do not have any current trip requests. Please visit Destinations to book your next trip!';
+    } else {
+      return pendingTrips;
+    }
+  },
 
-	getApprovedTrips(data) {
-		const approvedTrips = data.filter((trip) => trip.status === 'approved');
-		if (approvedTrips.length < 1) {
-			return 'You have no approved Trips';
-		} else {
-			return approvedTrips;
-		}
-	},
+  getApprovedTrips(data) {
+    const approvedTrips = data.filter((trip) => trip.status === 'approved');
+    if (approvedTrips.length < 1) {
+      return 'You have no approved Trips';
+    } else {
+      return approvedTrips;
+    }
+  },
 
-	getPastTrips(data) {
-		const approvedTrips = this.getApprovedTrips(data);
-		if (approvedTrips.length < 1) {
-			return 'You have not taken any trips yet. Please visit Destinations to book your next adventure!';
-		} else {
-			// need to import/install dayjs to find dates before current date
-			return approvedTrips;
-		}
-	},
+  getPastTrips(data) {
+    const approvedTrips = this.getApprovedTrips(data);
+    if (approvedTrips.length < 1) {
+      return 'You have not taken any trips yet. Please visit Destinations to book your next adventure!';
+    } else {
+      approvedTrips.filter((trip) => {
+        return dayjs(trip.date).isBefore(dayjs());
+      });
+      return approvedTrips;
+    }
+  },
 };
