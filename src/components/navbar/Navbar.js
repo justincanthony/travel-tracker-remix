7b138086728736628223e5ba5d32ae90d1816d93
Justin Anthony <justincanthony@yahoo.com>
2021-09-30 22:37:51 -0600

    Update name

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
 					</li>
 					<br />
 					<li>
-						<Link className="links" to="/pending_trips/user/44">
+						<Link className="links" to="/pending_trips/user/38">
 							Pending Trips
 						</Link>
 					</li>
