c063bf9d14f2fd6d94b87518abf5d6369541cb19
Justin Anthony <justincanthony@yahoo.com>
2021-10-01 00:00:00 -0600

    Update file

---
+++ src/components/Pending_trips/PendingTrips.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import './PendingTrips.css';
+import { fetchTripsByID } from '../../apiCalls';
+import { filterData } from '../../utils';
+import { TripCard } from '../Trip_Card/TripCard';
+
+export const PendingTrips = ({ pendingTrips }) => {
+	const { id } = pendingTrips;
+
+	const [tripsPending, setTripsPending] = useState([]);
+	const [error, setError] = useState('');
+
+	const getTrips = () => {
+		fetchTripsByID(id)
+			.then((data) =>
+				setTripsPending(filterData.getPendingTrips(data.requestedTrips))
+			)
+			.catch((error) => setError(error.message));
+	};
+
+	useEffect(() => {
+		getTrips();
+	}, []);
+
+	const tripsPendingCards = tripsPending.map((trip) => (
+		<TripCard key={trip.id} trip={trip} />
+	));
+
+	return (
+		<section className="pendingTripsContainer">
+			<h2>Pending Trips</h2>
+			<div className="pendingTripsWrapper">{tripsPendingCards}</div>
+		</section>
+	);
+};
