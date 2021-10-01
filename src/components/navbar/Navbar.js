cc929f8c38eeb747587deb676d4e3e75eb65d2b7
Justin Anthony <justincanthony@gmail.com>
2021-10-01 12:24:42 -0600

    Auto formatting

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,32 +3,32 @@ import './Navbar.css';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-	return (
-		<header className="header">
-			<h1 className="appTitle">Travel Tracker v2</h1>
-			<div className="linksContainer">
-				<ul className="linksList">
-					<li>
-						<Link className="links" to="/destinations">
-							Destinations
-						</Link>
-					</li>
-					<br />
-					<li>
-						<Link className="links" to="/pending_trips/user/38">
-							Pending Trips
-						</Link>
-					</li>
-					<br />
-					<li>
-						<Link className="links" to="/past_trips/user/38">
-							Past Trips
-						</Link>
-					</li>
-				</ul>
-			</div>
-		</header>
-	);
+  return (
+    <header className="header">
+      <h1 className="appTitle">Travel Tracker v2</h1>
+      <div className="linksContainer">
+        <ul className="linksList">
+          <li>
+            <Link className="links" to="/destinations">
+              Destinations
+            </Link>
+          </li>
+          <br />
+          <li>
+            <Link className="links" to="/pending_trips/user/38">
+              Pending Trips
+            </Link>
+          </li>
+          <br />
+          <li>
+            <Link className="links" to="/past_trips/user/38">
+              Past Trips
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
