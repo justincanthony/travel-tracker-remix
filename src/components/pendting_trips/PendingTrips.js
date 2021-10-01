2a96a027d8caa4ff71741fb5e1e1fd0bec0f95ad
Justin Anthony <justincanthony@yahoo.com>
2021-09-30 18:20:24 -0600

    create function to get trips by userID

--- src/components/pendting_trips/PendingTrips.js
+++ src/components/pendting_trips/PendingTrips.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { fetchdata } from '../../apiCalls';
+import { fetchTripsByID } from '../../apiCalls';
 import TripCard from '../trip_cards/TripCard';
 
 export const PendingTrips = ({ pendingTrips }) => {
-	const { pending_trips, id } = pendingTrips;
+	const { id } = pendingTrips;
 
 	const [tripsPending, setTripsPending] = useState([]);
 	const [error, setError] = useState('');
 
 	const getPendingTrips = () => {
-		fetchdata(pending_trips, id)
+		fetchTripsByID(id)
 			.then((data) => console.log(data))
 			.catch((error) => setError(error.message));
 	};
@@ -18,14 +18,14 @@ export const PendingTrips = ({ pendingTrips }) => {
 		getPendingTrips();
 	}, []);
 
-	const tripsPendingCards = tripsPending.map((trip) => (
-		<TripCard key={trip.id} trip={trip} />
-	));
+	// const tripsPendingCards = tripsPending.map((trip) => (
+	// 	<TripCard key={trip.id} trip={trip} />
+	// ));
 
 	return (
 		<section>
 			<h2>Pending Trips</h2>
-			{tripsPendingCards}
+			{/* {tripsPendingCards} */}
 		</section>
 	);
 };
