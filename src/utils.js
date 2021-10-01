bec62bbfdc6e47c52ce142a93de1ee4685ce37be
Justin Anthony <justincanthony@yahoo.com>
2021-09-30 22:27:03 -0600

    Adjust filter methods

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,17 @@
 export const filterData = {
 	getPendingTrips(data) {
-		const pendingTrips = data.filter(
-			(trip) => trip.status === 'pending' && trip.date > Date.now()
-		);
-		if (!pendingTrips) {
+		const pendingTrips = data.filter((trip) => trip.status === 'pending');
+
+		if (pendingTrips.length < 1) {
 			return 'You do not have any current trip requests. Please visit Destinations to book your next trip!';
+		} else {
+			return pendingTrips;
 		}
-		return data.filter((trip) => trip.status === 'pending');
 	},
 
 	getApprovedTrips(data) {
 		const approvedTrips = data.filter((trip) => trip.status === 'approved');
-		if (!approvedTrips) {
+		if (approvedTrips.length < 1) {
 			return 'You have no approved Trips';
 		} else {
 			return approvedTrips;
@@ -20,10 +20,11 @@ export const filterData = {
 
 	getPastTrips(data) {
 		const approvedTrips = this.getApprovedTrips(data);
-		if (!approvedTrips) {
+		if (approvedTrips.length < 1) {
 			return 'You have not taken any trips yet. Please visit Destinations to book your next adventure!';
 		} else {
-			return approvedTrips.filter((trip) => trip.date < Date.now());
+			// need to import/install dayjs to find dates before current date
+			return approvedTrips;
 		}
 	},
 };
