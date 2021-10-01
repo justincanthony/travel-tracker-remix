6b8a3eaf55fe642c24a5bac5fd8ae0e1ea6760d7
Justin Anthony <justincanthony@yahoo.com>
2021-10-01 09:45:30 -0600

    Create past trips component

--- src/components/past_trips/PastTrips.js
+++ src/components/past_trips/PastTrips.js
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchTripsByID } from '../../apiCalls';
+import { filterData } from '../../utils';
+import { TripCard } from '../Trip_Card/TripCard';
 import './PastTrips.css';
 
+export const PastTrips = ({ pastTrips }) => {
+	const { id } = pastTrips;
 
-export const PastTrips = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	const [tripsPast, setTripsPast] = useState([]);
+
+	const getTrips = () => {
+		fetchTripsByID(id).then((data) =>
+			setTripsPast(filterData.getPastTrips(data.requestedTrips))
+		);
+	};
+
+	useEffect(() => {
+		getTrips();
+	}, []);
+
+	const pastTripCards = tripsPast.map((trip) => {
+		return <TripCard key={trip.id} trip={trip} />;
+	});
+
+	return (
+		<section className="pastTripsContainer">
+			<h3>Past Trips</h3>
+			<section className="pastTripsCardWrapper">{pastTripCards}</section>
+		</section>
+	);
+};
