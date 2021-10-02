9a1dca50d5710e31544189537f16361f7c66694f
Justin Anthony <justincanthony@yahoo.com>
2021-10-02 09:57:34 -0600

    Add post request and handleSubmit

--- src/components/new_trip_form/NewTripForm.js
+++ src/components/new_trip_form/NewTripForm.js
@@ -1,19 +1,41 @@
 import React, { useState } from 'react';
+import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
 import './NewTripForm.css';
 
-const NewTripForm = ({userID, destinationID, }) => {
-	const [destination, setDestination] = useState('');
-  const [travelers, setTravelers] = useState('');
-  const [duration, setDuration] = useState('');
-  const [date, setDate] = useState('');
+const NewTripForm = ({ userID, destinationID }) => {
+	const [travelers, setTravelers] = useState('');
+	const [duration, setDuration] = useState('');
+	const [date, setDate] = useState('');
 
+	const sendNewTrip = (newTrip) => {
+		const requestOptions = {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(newTrip),
+		};
+		fetch('http://localhost3001/api/v1/trips', requestOptions)
+			.then((res) => res.json())
+			.then((data) => console.log(data));
+	};
 
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const newTrip = {
+			id: Date.now(),
+			userID,
+			destinationID,
+			duration,
+			travelers,
+			status: 'pending',
+			suggestedActivites: [],
+		};
+		scryRenderedComponentsWithType();
+	};
 
 	return <form className="newTripForm" type="submit"></form>;
 };
 
 export default NewTripForm;
-
 
 // const requiredProperties = [
 //   'id',
