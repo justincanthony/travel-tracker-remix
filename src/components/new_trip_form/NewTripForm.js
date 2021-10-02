a8fa0555e60584930ea610a2992e4507de3a9595
Justin Anthony <justincanthony@yahoo.com>
2021-10-02 09:46:25 -0600

    Create state properties

--- src/components/new_trip_form/NewTripForm.js
+++ src/components/new_trip_form/NewTripForm.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewTripForm.css';
 
-const NewTripForm = () => {
-  return <form className="newTripForm" type="submit"></form>;
+const NewTripForm = ({userID, destinationID, }) => {
+	const [destination, setDestination] = useState('');
+  const [travelers, setTravelers] = useState('');
+  const [duration, setDuration] = useState('');
+  const [date, setDate] = useState('');
+
+
+
+	return <form className="newTripForm" type="submit"></form>;
 };
 
 export default NewTripForm;
+
+
+// const requiredProperties = [
+//   'id',
+//   'userID',
+//   'destinationID',
+//   'travelers',
+//   'date',
+//   'duration',
+//   'status',
+//   'suggestedActivities',
+// ];
