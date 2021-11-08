a20a6b62891935d1b5b768f717ca09744d98fba1
Justin Anthony <justincanthony@gmail.com>
2021-11-07 17:31:33 -0700

    Create fetch traveler call

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,9 +1,16 @@
 export const fetchData = async (path) => {
-	const res = await fetch(`http://localhost:3001/api/v1/${path}`);
-	return res.json();
+  const res = await fetch(`http://localhost:3001/api/v1/${path}`);
+  return res.json();
 };
 
 export const fetchTripsByID = async (id) => {
-	const res = await fetch(`http://localhost:3001/api/v1/trips/${id}`);
-	return res.json();
+  const res = await fetch(`http://localhost:3001/api/v1/trips/${id}`);
+  return res.json();
+};
+
+export const fetchTraveler = async (username, password) => {
+  const res = await fetch(
+    `http://localhost:3001/api/v1/${username}/${password}`
+  );
+  return res.json();
 };
