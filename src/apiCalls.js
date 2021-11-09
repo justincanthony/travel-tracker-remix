d29ab17c85b55835b68f31e480df63557426b2bf
Justin Anthony <justincanthony@gmail.com>
2021-11-08 23:22:09 -0700

    Add error handling to all fetch calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,23 @@
-export const fetchData = async (path) => {
-  const res = await fetch(`http://localhost:3001/api/v1/${path}`);
-  return res.json();
+export const fetchData = async () => {
+  const response = await fetch('http://localhost:3001/api/v1/destinations');
+  if (response.status >= 400) {
+    return response.json().then((res) => {
+      throw new Error(res.message);
+    });
+  } else {
+    return response.json();
+  }
 };
 
 export const fetchTripsByID = async (id) => {
-  const res = await fetch(`http://localhost:3001/api/v1/trips/${id}`);
-  return res.json();
+  const response = await fetch(`http://localhost:3001/api/v1/trips/${id}`);
+  if (response.status >= 400) {
+    return response.json().then((res) => {
+      throw new Error(res.message);
+    });
+  } else {
+    return response.json();
+  }
 };
 
 export const fetchTraveler = async (username, password) => {
