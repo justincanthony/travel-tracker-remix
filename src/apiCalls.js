6d3b75d57021f30fa2c4a7644be25a68e9d559ec
Justin Anthony <justincanthony@gmail.com>
2021-11-09 12:09:53 -0700

    Add error handling

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -34,13 +34,24 @@ export const fetchTraveler = async (username, password) => {
   }
 };
 
-export const bookTrip = (newTrip) => {
+export const bookTrip = async (newTrip) => {
   const requestOptions = {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(newTrip),
   };
-  fetch('http://localhost:3001/api/v1/trips', requestOptions);
+
+  const response = await fetch(
+    'http://localhost:3001/api/v1/trips',
+    requestOptions
+  );
+  if (response.status >= 400) {
+    return response.json().then((res) => {
+      throw new Error(res.message);
+    });
+  } else {
+    return response.json();
+  }
 };
 
 // Different Version of Fetch
