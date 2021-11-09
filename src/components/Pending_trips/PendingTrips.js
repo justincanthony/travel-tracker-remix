aca7bad76d48ed156b3505b48e1a1b61a7db60cf
Justin Anthony <justincanthony@gmail.com>
2021-11-08 22:38:58 -0700

    Remove state and functions

--- src/components/Pending_trips/PendingTrips.js
+++ src/components/Pending_trips/PendingTrips.js
@@ -1,54 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './PendingTrips.css';
-import { fetchTripsByID } from '../../apiCalls';
-import { filterData } from '../../utils';
 import { TripCard } from '../Trip_Card/TripCard';
-import { toast } from 'react-toastify';
-import { ErrorMessage } from '../Error_Message/ErrorMessage';
 
-export const PendingTrips = ({ pendingTrips }) => {
-	const { id } = pendingTrips;
-	const [tripsPending, setTripsPending] = useState([]);
-	const [error, setError] = useState('');
-	const [notification, setNotification] = useState('');
+export const PendingTrips = ({ tripsPending, cancelTrip }) => {
+  const tripsPendingCards = tripsPending.map((trip) => {
+    return <TripCard key={trip.id} trip={trip} cancelTrip={cancelTrip} />;
+  });
 
-	const getTrips = () => {
-		fetchTripsByID(id)
-			.then((data) =>
-				setTripsPending(filterData.getPendingTrips(data.requestedTrips))
-			)
-			.catch((error) => setError(error.message));
-	};
-
-	const cancelTrip = (id) => {
-		fetch(`http://localhost:3001/api/v1/trips/${id}`, { method: 'DELETE' })
-			.then((res) => res.json())
-			.then((data) => setNotification(data.message))
-			.catch((error) => setError(error.message));
-	};
-
-	useEffect(() => {
-		getTrips();
-	}, []);
-
-	useEffect(() => {
-		getTrips();
-	}, [notification]);
-
-	let tripsPendingCards;
-	if (typeof tripsPending !== 'string') {
-		tripsPendingCards = tripsPending.map((trip) => {
-			return <TripCard key={trip.id} trip={trip} cancelTrip={cancelTrip} />;
-		});
-	} else {
-		tripsPendingCards = <ErrorMessage message={tripsPending} />;
-	}
-
-	return (
-		<section className="pendingTripsContainer">
-			<h2>Pending Trips</h2>
-			{!error && <div className="pendingTripsWrapper">{tripsPendingCards}</div>}
-			{notification && <ErrorMessage message={notification} />}
-		</section>
-	);
+  return (
+    <section className="pendingTripsContainer">
+      <h2>Pending Trips</h2>
+      <div className="pendingTripsWrapper">{tripsPendingCards}</div>
+    </section>
+  );
 };
