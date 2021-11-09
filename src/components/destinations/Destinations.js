80171f9b230b5ac2cbba76cd2ea3ed68eac17ca2
Justin Anthony <justincanthony@gmail.com>
2021-11-08 23:21:32 -0700

    Remove state from component

--- src/components/destinations/Destinations.js
+++ src/components/destinations/Destinations.js
@@ -1,61 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { fetchData } from '../../apiCalls';
 import DestinationCard from '../Destination_Card/DestinationCard';
 import './Destinations.css';
 
-export const Destinations = (params) => {
-	const [destinations, setDestinations] = useState([]);
-	const [error, setError] = useState('');
-	const [notification, setNotification] = useState('');
+export const Destinations = ({ destinations, userID, sendNewTrip }) => {
+  const destinationCards = destinations.map((destinationObj) => (
+    <DestinationCard
+      key={destinationObj.id}
+      destinationObj={destinationObj}
+      sendNewTrip={sendNewTrip}
+      userID={userID}
+    />
+  ));
 
-	const getDestinations = () => {
-		fetchData(params.destinations)
-			.then((data) => setDestinations(data.destinations))
-			.catch((error) => setError(error.message));
-	};
-
-	const sendNewTrip = (newTrip, destination) => {
-		const requestOptions = {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify(newTrip),
-		};
-
-		toast.promise(
-			fetch('http://localhost:3001/api/v1/trips', requestOptions)
-				.then((res) => res.json())
-				.then((data) => setNotification(data.message))
-				.catch((error) => setError(error.message)),
-			{
-				pending: 'Please wait while we make your request',
-				success: `Your trip request to ${destination} has been made`,
-				error: 'Whoops! Something went wrong. Please try again',
-			}
-		);
-	};
-
-	const destinationCards = destinations.map((destinationObj) => (
-		<DestinationCard
-			key={destinationObj.id}
-			destinationObj={destinationObj}
-			sendNewTrip={sendNewTrip}
-		/>
-	));
-
-	useEffect(() => {
-		getDestinations();
-	}, []);
-
-	return (
-		<section className="destinationsContainer">
-			<h2>Destinations</h2>
-			<div className="destinationsCardWrapper">{destinationCards}</div>
-		</section>
-	);
+  return (
+    <section className="destinationsContainer">
+      <h2>Destinations</h2>
+      <div className="destinationsCardWrapper">{destinationCards}</div>
+    </section>
+  );
 };
 
 export default Destinations;
-
-
