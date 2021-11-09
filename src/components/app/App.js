253db6e2d92d6962fde5e9ab0c3ef247c224d957
Justin Anthony <justincanthony@gmail.com>
2021-11-08 18:44:23 -0700

    Import login and create route

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,43 +2,45 @@ import Destinations from '../Destinations/Destinations';
 import Navbar from '../Navbar/Navbar';
 import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import { Login } from '../Login/Login';
 import { PendingTrips } from '../Pending_trips/PendingTrips';
 import { PastTrips } from '../Past_Trips/PastTrips';
 import { ToastContainer } from 'react-toastify';
 
 const App = () => {
-	return (
-		<div className="App">
-			<ToastContainer />
-			<Navbar />
-			<Switch>
-				<Route
-					exact
-					path="/:destinations"
-					render={({ match }) => {
-						const { params } = match;
-						return <Destinations destinations={params.destinations} />;
-					}}
-				/>
-				<Route
-					exact
-					path="/pending_trips/user/:id"
-					render={({ match }) => {
-						const { params } = match;
-						return <PendingTrips pendingTrips={params} />;
-					}}
-				/>
-				<Route
-					exact
-					path="/past_trips/user/:id"
-					render={({ match }) => {
-						const { params } = match;
-						return <PastTrips pastTrips={params} />;
-					}}
-				/>
-			</Switch>
-		</div>
-	);
+  return (
+    <div className="App">
+      <ToastContainer />
+      <Navbar />
+      <Switch>
+        <Route exact path="/" render={() => <Login />} />
+        <Route
+          exact
+          path="/:destinations"
+          render={({ match }) => {
+            const { params } = match;
+            return <Destinations destinations={params.destinations} />;
+          }}
+        />
+        <Route
+          exact
+          path="/pending_trips/user/:id"
+          render={({ match }) => {
+            const { params } = match;
+            return <PendingTrips pendingTrips={params} />;
+          }}
+        />
+        <Route
+          exact
+          path="/past_trips/user/:id"
+          render={({ match }) => {
+            const { params } = match;
+            return <PastTrips pastTrips={params} />;
+          }}
+        />
+      </Switch>
+    </div>
+  );
 };
 
 export default App;
