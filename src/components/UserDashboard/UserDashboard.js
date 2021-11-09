43f5f10f7978a8aebf7c79366b86a80bbde87aad
Justin Anthony <justincanthony@gmail.com>
2021-11-08 20:37:56 -0700

    Import componets

--- src/components/UserDashboard/UserDashboard.js
+++ src/components/UserDashboard/UserDashboard.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import './UserDashboard.css';
+import Destinations from '../Destinations/Destinations';
+import Navbar from '../Navbar/Navbar';
+import { PendingTrips } from '../Pending_trips/PendingTrips';
+import { PastTrips } from '../Past_Trips/PastTrips';
 
 export const UserDashboard = () => {
   return <section className="userDashboardContainer"></section>;
