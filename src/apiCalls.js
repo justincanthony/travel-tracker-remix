0241086dae244ad9605aa13c4f07f33e3dee3325
Justin Anthony <justincanthony@gmail.com>
2021-11-08 18:23:58 -0700

    Refactor fetch to handle error correctly

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,8 +9,28 @@ export const fetchTripsByID = async (id) => {
 };
 
 export const fetchTraveler = async (username, password) => {
-  const res = await fetch(
-    `http://localhost:3001/api/v1/${username}/${password}`
+  const response = await fetch(
+    `http://localhost:3001/api/v1/travelers/${username}/${password}`
   );
-  return res.json();
+  if (response.status >= 400) {
+    return response.json().then((res) => {
+      throw new Error(res.message);
+    });
+  } else {
+    return response.json();
+  }
 };
+
+// Different Version of Fetch
+// export const fetchTraveler = (username, password) => {
+//   return fetch(
+//     `http://localhost:3001/api/v1/travelers/${username}/${password}`
+//   ).then((response) => {
+//     if (response.status >= 400) {
+//       return response.json().then((res) => {
+//         throw new Error(res.message);
+//       });
+//     } else {
+//       return response.json();
+//     }
+//   });
