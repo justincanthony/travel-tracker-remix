7202b177b1d538ce91e47da25e05cf8cdcd8ce23
Justin Anthony <justincanthony@gmail.com>
2021-11-17 16:13:53 -0700

    Add hooks and functions

--- src/components/Pending_trips/PendingTrips.js
+++ src/components/Pending_trips/PendingTrips.js
@@ -1,26 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './PendingTrips.css';
+import { filterData } from '../../utils';
+import { fetchTripsByID, deleteTrip } from '../../apiCalls';
 import { ErrorMessage } from '../Error_Message/ErrorMessage';
 import { TripCard } from '../Trip_Card/TripCard';
 
-export const PendingTrips = ({ tripsPending, cancelTrip }) => {
+export const PendingTrips = ({ userID }) => {
+  const [tripsPending, setTripsPending] = useState([]);
+  const [notification, setNotification] = useState('');
   const [error, setError] = useState('');
-  const [isLoading, setIsLoading] = useState('true');
+  const [isLoading, setIsLoading] = useState(true);
 
-  let tripsPendingCards;
-  if (tripsPending.length > 0) {
-    setIsLoading(false);
-    tripsPendingCards = tripsPending.map((trip) => {
-      return <TripCard key={trip.id} trip={trip} cancelTrip={cancelTrip} />;
-    });
-  } else {
-    setError('Looks Like you need to book some trips!');
-  }
+  const cancelTrip = (id) => {
+    deleteTrip(id)
+      .then((data) => setNotification(data.message))
+      .catch((error) => setError(error.message));
+  };
+
+  const getTrips = () => {
+    fetchTripsByID(userID)
+      .then((data) => {
+        setTripsPending(filterData.getPendingTrips(data.requestedTrips));
+        setIsLoading(false);
+      })
+      .catch((error) => setError(error.message));
+  };
+
+  useEffect(() => {
+    getTrips();
+  }, []);
+
+  const tripsPendingCards = tripsPending.map((trip) => {
+    return (
+      <TripCard
+        key={trip.id}
+        trip={trip}
+        userID={userID}
+        cancelTrip={cancelTrip}
+      />
+    );
+  });
 
   return (
     <section className="pendingTripsContainer">
       {isLoading && !error && <p>"We are getting your trips...</p>}
-      {tripsPending.length > 0 && !error && (
+      {!isLoading && !error && (
         <React.Fragment>
           <h2>Pending Trips</h2>
           <div className="pendingTripsWrapper">{tripsPendingCards}</div>
