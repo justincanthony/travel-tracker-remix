c11f1cf684b4216fcb25a7da820f22b1087fba7e
Justin Anthony <justincanthony@gmail.com>
2021-11-17 18:35:18 -0700

    Adjust routes

--- src/components/Dashboard_Navbar/DashboardNavbar.js
+++ src/components/Dashboard_Navbar/DashboardNavbar.js
@@ -2,26 +2,26 @@ import React from 'react';
 import './DashboardNavbar.css';
 import { Link } from 'react-router-dom';
 
-export const DashboardNavbar = () => {
+export const DashboardNavbar = ({ userID }) => {
   return (
-    <header className="dashBoardNavbar">
+    <header className="dashboardNavbar">
       <h1 className="appTitle">Travel Tracker v2</h1>
       <div className="linksContainer">
         <ul className="linksList">
           <li>
-            <Link className="links" to="/destinations">
+            <Link className="links" to={`/destinations/${userID}`}>
               Destinations
             </Link>
           </li>
           <br />
           <li>
-            <Link className="links" to="/pending_trips/user/38">
+            <Link className="links" to={`/user_dashboard/${userID}`}>
               Pending Trips
             </Link>
           </li>
           <br />
           <li>
-            <Link className="links" to="/past_trips/user/38">
+            <Link className="links" to={`/pastTrips/${userID}`}>
               Past Trips
             </Link>
           </li>
