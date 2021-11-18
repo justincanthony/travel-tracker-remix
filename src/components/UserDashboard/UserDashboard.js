ee7fa4ba95ce10b181e696d77107f1bc1c48edb2
Justin Anthony <justincanthony@gmail.com>
2021-11-17 18:38:01 -0700

    Remove fucntions and change return

--- src/components/UserDashboard/UserDashboard.js
+++ src/components/UserDashboard/UserDashboard.js
@@ -1,58 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './UserDashboard.css';
 import Destinations from '../Destinations/Destinations';
-import Navbar from '../Navbar/Navbar';
-import { toast } from 'react-toastify';
-import { filterData } from '../../utils';
-import { bookTrip, fetchTripsByID, deleteTrip } from '../../apiCalls';
-
+import { DashboardNavbar } from '../Dashboard_Navbar/DashboardNavbar';
+import { ErrorMessage } from '../Error_Message/ErrorMessage';
 import { PendingTrips } from '../Pending_trips/PendingTrips';
-// import { PastTrips } from '../Past_Trips/PastTrips';
+import { PastTrips } from '../Past_Trips/PastTrips';
 
-export const UserDashboard = ({ userID }) => {
-  const [tripsPending, setTripsPending] = useState([]);
-  const [notification, setNotification] = useState('');
-  const [error, setError] = useState('');
-
-  const sendNewTrip = (newTrip, destination) => {
-    toast.promise(
-      bookTrip(newTrip)
-        .then((data) => console.log(data.message))
-        .catch((error) => setError(error.message)),
-      {
-        pending: 'Please wait while we make your request',
-        success: `Your trip request to ${destination} has been made`,
-        error: 'Whoops! Something went wrong. Please try again',
-      }
-    );
-  };
-
-  const getTrips = () => {
-    fetchTripsByID(userID)
-      .then((data) =>
-        setTripsPending(filterData.getPendingTrips(data.requestedTrips))
-      )
-      .catch((error) => setError(error.message));
-  };
-
-  const cancelTrip = (id) => {
-    deleteTrip(id)
-      .then((data) => setNotification(data.message))
-      .catch((error) => setError(error.message));
-  };
-
-  useEffect(() => {
-    getTrips();
-  }, []);
-
+export const UserDashboard = ({ userID, type }) => {
   return (
     <React.Fragment>
-      <Navbar />
-      <Destinations userID={userID} sendNewTrip={sendNewTrip} />
-      {/* {tripsPending && (
-        <PendingTrips tripsPending={tripsPending} cancelTrip={cancelTrip} />
-      )} */}
-      {/* <PastTrips userID={userID} /> */}
+      <DashboardNavbar userID={userID} />
+      {type === 'pendingTrips' && <PendingTrips userID={userID} />}
+      {type === 'destinations' && <Destinations userID={userID} />}
+      {type === 'pastTrips' && <PastTrips userID={userID} />}
     </React.Fragment>
   );
 };
