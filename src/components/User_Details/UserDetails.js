fc0df63d3681a10e3d59a0361a9f8cb494c003a5
Justin Anthony <justincanthony@gmail.com>
2021-11-18 13:06:20 -0700

    Add id to fetch more info possibly

--- src/components/User_Details/UserDetails.js
+++ src/components/User_Details/UserDetails.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './UserDetails.css';
 
 export const UserDetails = ({ traveler }) => {
-  const { name, travelerType } = traveler;
+  const { id, name, travelerType } = traveler;
 
   return (
     <section className="userDetails">
       <h3 className="userName">Welcome {name}</h3>
-      <h4 className="travelerType">Traveler Type: {travelerType}</h4>
+      {/* <h4 className="travelerType">Traveler Type: {travelerType}</h4> */}
     </section>
   );
 };
