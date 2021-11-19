f425d05a6ebc42f4d687a305aec37bf81ba01254
Justin Anthony <justincanthony@gmail.com>
2021-11-18 18:21:51 -0700

    Add elems for styling

--- src/components/Dashboard_Navbar/DashboardNavbar.js
+++ src/components/Dashboard_Navbar/DashboardNavbar.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './DashboardNavbar.css';
 import { Link } from 'react-router-dom';
+import PersonIcon from '@mui/icons-material/Person';
 
-export const DashboardNavbar = ({ userID }) => {
+export const DashboardNavbar = ({ userID, traveler }) => {
+  const { name } = traveler;
+
   return (
     <header className="dashboardNavbar">
-      <h1 className="appTitle">Travel Tracker v2</h1>
+      <div className="titleMenuWrapper">
+        <div className="titleContainer">
+          <h1 className="appTitle">Travel Tracker v2</h1>
+        </div>
+        <div className="userIconWrapper">
+          <PersonIcon />
+          <br />
+          <p className="userName">{name}</p>
+        </div>
+      </div>
       <div className="linksContainer">
         <ul className="linksList">
+          <li>
+            <Link className="links" to={`/user_dashboard/${userID}`}>
+              Dashboard
+            </Link>
+          </li>
+          <br />
           <li>
             <Link className="links" to={`/destinations/${userID}`}>
               Destinations
