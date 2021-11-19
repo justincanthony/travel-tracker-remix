3cfe862ee41fe019017615d6a232495117950ebb
Justin Anthony <justincanthony@gmail.com>
2021-11-19 12:39:19 -0700

    Add useEffect to display upcoming trip details

--- src/components/User_Details/UserDetails.js
+++ src/components/User_Details/UserDetails.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchTripsByID } from '../../apiCalls';
+import { filterData } from '../../utils';
 import './UserDetails.css';
 
 export const UserDetails = ({ traveler }) => {
   const { id, name, travelerType } = traveler;
 
+  const [tripPending, setTripPending] = useState([]);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const getTrips = () => {
+      fetchTripsByID(id)
+        .then((data) => {
+          setTripPending(filterData.getUpcomingTrip(data.requestedTrips));
+        })
+        .catch((error) => setError(error.message));
+    };
+    getTrips();
+  }, [id]);
+
   return (
     <section className="userDetails">
       <h3 className="userName">Welcome {name}</h3>
+      <h4>Your Upcoming Trip</h4>
+      <p>{tripPending.date}</p>
+      <p>{tripPending.place.destination}</p>
+      <p>{tripPending.duration}</p>
+      <p>{tripPending.travelers}</p>
+
       {/* <h4 className="travelerType">Traveler Type: {travelerType}</h4> */}
     </section>
   );
