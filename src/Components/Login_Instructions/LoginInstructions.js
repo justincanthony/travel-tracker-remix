655b1422a74ea25f700a78ea00f3aa56d7669cfc
Justin Anthony <justincanthony@gmail.com>
2021-11-27 20:48:51 -0700

    Add login instructions components

---
+++ src/Components/Login_Instructions/LoginInstructions.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const LoginInstructions = () => {
+  return (
+    <section className="loginInstructions">
+      <h2>How to Log In</h2>
+      <p>Username can be the word "traveler" followed by any number 1-50</p>
+      <br />
+      <p>Example: Traveler50</p>
+      <br />
+      <p>Please use the password "traveler" for all usernames</p>
+    </section>
+  );
+};
