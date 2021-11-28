221d5e9735f7115d2cfc327e8cfa56e6124367fe
Justin Anthony <justincanthony@gmail.com>
2021-11-27 20:56:01 -0700

    Add link and adjust password case

--- src/Components/Login_Instructions/LoginInstructions.js
+++ src/Components/Login_Instructions/LoginInstructions.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './LoginInstructions.css';
 
 export const LoginInstructions = () => {
   return (
@@ -6,9 +8,10 @@ export const LoginInstructions = () => {
       <h2>How to Log In</h2>
       <p>Username can be the word "traveler" followed by any number 1-50</p>
       <br />
-      <p>Example: Traveler50</p>
+      <p>Example: traveler50</p>
       <br />
       <p>Please use the password "traveler" for all usernames</p>
+      <Link to="/">Return to Log In</Link>
     </section>
   );
 };
